4118b67e0d5dd8e4b3c96eb6b38bcf75e8a28fa1
Rodrigo Lúcio <luciodigo@gmail.com>
2023-05-29 21:42:52 -0300

    Finalizando sobre docker compose

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host:'db',
+    user: 'root',
+    password : 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `insert into people (name) values ('Rodrigo')`;
+connection.query(sql);
+connection.end();
+
+
 
 app.get('/', (req, res) => {
-    res.send("<h1>Full cycle nodejs</h1>")
+    res.send("<h1>Full cycle nodejs 1</h1>")
 })
 
 app.listen(port, () => {
