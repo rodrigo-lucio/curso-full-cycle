c0f5976c9f9aa7ca1d78c08feaaef35667fa04ee
Rodrigo Lúcio <luciodigo@gmail.com>
2023-06-22 22:53:54 -0300

    Criado resolver para buscar categoria pelo ID do curso

--- 03-comunicacao-entre-sistemas/graphql/internal/database/category.go
+++ 03-comunicacao-entre-sistemas/graphql/internal/database/category.go
@@ -45,3 +45,13 @@ func (c *Category) FindAll() ([]Category, error) {
 	}
 	return categories, nil
 }
+
+func (c *Category) FindByCourseId(courseId string) (Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT c.id, c.name, c.description FROM categories c INNER JOIN courses co on c.id = co.category_id WHERE co.id = $1", courseId).
+		Scan(&id, &name, &description)
+	if err != nil {
+		return Category{}, err
+	}
+	return Category{ID: id, Name: name, Description: description}, nil
+}
