88d9a3c6e9e51fdee47a19c81cf879d0854874fa
Rodrigo Lúcio <luciodigo@gmail.com>
2023-06-27 23:00:58 -0300

    Criado chamada gRPC para criar a listar categorias

---
+++ 03-comunicacao-entre-sistemas/grpc/internal/service/category.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"context"
+
+	"github.com/devfullcycle/14-gRPC/internal/database"
+	"github.com/devfullcycle/14-gRPC/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDb database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDb,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
+
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
