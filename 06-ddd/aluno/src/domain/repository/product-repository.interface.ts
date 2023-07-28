0ded05384ec14cd838dfcbc222b3c6bb9f5e2c72
Rodrigo Lúcio <luciodigo@gmail.com>
2023-07-27 22:53:30 -0300

    Ajustado imports

--- 06-ddd/aluno/src/domain/repository/product-repository.interface.ts
+++ 06-ddd/aluno/src/domain/repository/product-repository.interface.ts
@@ -1,7 +1,7 @@
 import Product from "../entity/product";
 import RepositoryInterface from "./repository-interface";
 
-export default interface ProductRepository extends RepositoryInterface<Product> {
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product> {
 
 }
 
