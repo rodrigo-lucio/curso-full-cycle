48214d4a40ff62b4ca9ca4497da1886a7cb260d4
Rodrigo Lúcio <luciodigo@gmail.com>
2023-08-10 22:51:35 -0300

    Criando factory do Customer

---
+++ 06-ddd/aluno/src/domain/customer/factory/customer.factory.ts
@@ -0,0 +1,18 @@
+import Address from "../entity/address";
+import Customer from "../entity/customer";
+import { v4 as uuid } from "uuid";
+
+export default class CustomerFactory {
+    static createWithAddress(name: string, address: Address): Customer {
+        const customer = new Customer(uuid(), name);
+        customer.changeAddress(address);
+        customer.activate();
+        return customer;
+    }
+    public static create(name: string): Customer {
+        return new Customer(uuid(), name);
+    }
+
+
+
+}
