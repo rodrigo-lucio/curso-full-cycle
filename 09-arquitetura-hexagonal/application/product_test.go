6c2cc3b9626dad8501fd80368e6cf28f4118dfa9
Rodrigo Lúcio <luciodigo@gmail.com>
2023-08-28 22:47:39 -0300

    Iniciando a parte prática do módulo de arquitetura hexagonal

---
+++ 09-arquitetura-hexagonal/application/product_test.go
@@ -0,0 +1,21 @@
+package application_test
+
+import (
+	"github.com/codedu/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "IPHONE"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
---
+++ 09-arquitetura-hexagonal/go.mod
@@ -0,0 +1,5 @@
+module github.com/codedu/go-hexagonal
+
+go 1.16
+
+require github.com/stretchr/testify v1.8.4
