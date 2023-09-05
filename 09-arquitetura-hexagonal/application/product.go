bfd0217e45f4a24f37c10e13caaf57ebdf195f86
Rodrigo Lúcio <luciodigo@gmail.com>
2023-09-04 22:46:37 -0300

    Ajuste Mocks

--- 09-arquitetura-hexagonal/application/product.go
+++ 09-arquitetura-hexagonal/application/product.go
@@ -24,8 +24,8 @@ type ProductInterface interface {
 type ProductServiceInterface interface {
 	Get(id string) (ProductInterface, error)
 	Create(name string, price float64) (ProductInterface, error)
-	Enable(product ProductInterface) (ProductServiceInterface, error)
-	Disable(product ProductInterface) (ProductServiceInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
 }
 
 type ProductReader interface {
