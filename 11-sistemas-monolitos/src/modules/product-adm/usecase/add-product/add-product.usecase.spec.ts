78037e8b6090321434d7eedd314270c8986a7973
Rodrigo Lúcio <luciodigo@gmail.com>
2024-01-17 22:27:45 -0300

    Criando o usecase addProduct

---
+++ 11-sistemas-monolitos/src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,34 @@
+import AddProductUseCase from "./add-product.usecase";
+
+describe("Add Product usecase unit test", () => {
+
+    const MockRepository = () => {
+        return {
+            add: jest.fn(),
+            find: jest.fn(),
+        };
+    };
+
+    it("should add a product", async () => {
+        const productRepository = MockRepository();
+        const usecase = new AddProductUseCase(productRepository);
+        
+        const input = {
+            name: "Product Name",
+            description: "Product Description",
+            purchasePrice: 100,
+            stock: 10
+        };
+        
+        const output = await usecase.execute(input);
+
+        expect(productRepository.add).toBeCalled()
+        expect(output.id).toBeDefined();
+        expect(output.name).toBe(input.name);
+        expect(output.description).toBe(input.description);
+        expect(output.purchasePrice).toBe(input.purchasePrice);
+        expect(output.stock).toBe(input.stock);
+
+    });
+
+});
