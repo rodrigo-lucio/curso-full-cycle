e27c562f69b9cecbc639e6fdef9a811fc05fa11c
Rodrigo Lúcio <luciodigo@gmail.com>
2024-01-17 22:29:06 -0300

    Criando o usecase checkStock

---
+++ 11-sistemas-monolitos/src/modules/product-adm/usecase/check-stock/check-stock.dto.ts
@@ -0,0 +1,9 @@
+export interface CheckStockInputDto {
+    productId: string;
+}
+
+export interface CheckStockOutputDto {
+    productId: string;
+    productName: string;
+    stock: number;
+}
---
+++ 11-sistemas-monolitos/src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,39 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity";
+import CheckStockUseCase from "./check-stock.usecase";
+
+const product = new Product({
+    id: new Id("1"),
+    name: "Product Name",
+    description: "Product Description",
+    purchasePrice: 100,
+    stock: 10
+});
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn().mockResolvedValue(Promise.resolve(product))
+    };
+};
+
+describe("Add Product usecase unit test", () => {
+    
+    it("should check a stock product", async () => {
+        const productRepository = MockRepository();
+        const usecaseStock = new CheckStockUseCase(productRepository);
+        const inputStock = {
+            productId: "1"
+        }        
+
+        const output = await usecaseStock.execute(inputStock);
+        
+        expect(productRepository.find).toBeCalledWith(new Id(inputStock.productId).id);
+        expect(productRepository.find).toHaveBeenCalled();
+        expect(output.productId).toBe(product.id.id);
+        expect(output.productName).toBe(product.name);
+        expect(output.stock).toBe(product.stock);
+
+    });
+
+});
