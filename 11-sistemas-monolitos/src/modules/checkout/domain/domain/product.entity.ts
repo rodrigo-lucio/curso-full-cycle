92345f1dd3cf5b02480da5fa1c6fc177ed7de745
Rodrigo Lúcio <luciodigo@gmail.com>
2024-02-28 23:02:32 -0300

    Finalizando os testes unitários de place order

---
+++ 11-sistemas-monolitos/src/modules/checkout/domain/domain/product.entity.ts
@@ -0,0 +1,36 @@
+import AggregateRoot from "../../../@shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../../@shared/domain/entity/base.entity";
+import Id from "../../../@shared/domain/value-object/id.value-object";
+
+type ProductProps = {
+    id: Id;
+    name: string;
+    description: string;
+    salePrice: number;
+}
+
+export default class Product extends BaseEntity implements AggregateRoot {
+    private _name: string;
+    private _description: string;
+    private _salePrice: number;
+
+    constructor(props: ProductProps) {
+        super(props.id);
+        this._name = props.name;
+        this._description = props.description;
+        this._salePrice = props.salePrice;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get salePrice() {
+        return this._salePrice;
+    }
+    
+}
