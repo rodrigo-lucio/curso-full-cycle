7274172c20fd0e68bf96030a408ac393498ccb99
Rodrigo Lúcio <luciodigo@gmail.com>
2024-09-12 23:21:28 -0300

    criado account_db e test

---
+++ 12-arq-microsservicos/internal/database/account_db_test.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"database/sql"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/entity"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type AccountDBTestSuite struct {
+	suite.Suite
+	db        *sql.DB
+	accountDb *AccountDB
+	client    *entity.Client
+}
+
+func (s *AccountDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("create table clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+	db.Exec("create table accounts (id varchar(255), client_id varchar(255), balance int, created_at date )")
+	s.accountDb = NewAccountDB(db)
+	s.client, _ = entity.NewClient("Joe", "j@j.com")
+
+}
+
+func (s *AccountDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("drop table clients")
+	s.db.Exec("drop table accounts")
+}
+
+func TestAccountTestSuite(t *testing.T) {
+	suite.Run(t, new(AccountDBTestSuite))
+}
+
+func (s *AccountDBTestSuite) TestSave() {
+	account := entity.NewAccount(s.client)
+	err := s.accountDb.Save(account)
+	s.Nil(err)
+}
+
+func (s *AccountDBTestSuite) TestFindByID() {
+
+	s.db.Exec("insert into clients (id, name, email, created_at) values (?, ?, ?, ?)",
+		s.client.ID, s.client.Name, s.client.Email, s.client.CreatedAt)
+
+	account := entity.NewAccount(s.client)
+	err := s.accountDb.Save(account)
+	s.Nil(err)
+	accountDB, err := s.accountDb.FindById(account.ID)
+	s.Nil(err)
+	s.Equal(account.ID, accountDB.ID)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Balance, accountDB.Balance)
+}
