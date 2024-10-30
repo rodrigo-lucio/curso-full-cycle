ce330805206dc2278bc0e5a347186d61054deb7d
Rodrigo Lúcio <luciodigo@gmail.com>
2024-10-29 23:11:20 -0300

    criado interfaces

---
+++ 13-event-driven-architecture/pkg/events/interfaces.go
@@ -0,0 +1,21 @@
+package events
+
+import "time"
+
+type EventInterface interface {
+	GetName() string
+	GetDateTime() time.Time
+	GetPayload() interface{}
+}
+
+type EventHandlerInterface interface {
+	Handle(event EventInterface)
+}
+
+type EventDispatcherInterface interface {
+	Register(eventName string, handler EventHandlerInterface)
+	Dispatch(event EventInterface)
+	Remove(eventName string, handler EventHandlerInterface)
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear()
+}
