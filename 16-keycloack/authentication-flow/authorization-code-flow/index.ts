146d0566d774db60dbe9ed2d4e6c0dcdcaf5c57c
Rodrigo Lúcio <luciodigo@gmail.com>
2025-11-03 22:39:48 -0300

    redirect

--- 16-keycloack/authentication-flow/authorization-code-flow/index.ts
+++ 16-keycloack/authentication-flow/authorization-code-flow/index.ts
@@ -15,6 +15,35 @@ app.get('/login', (req, res) => {
    res.redirect(url);
 });
 
+app.get('/callback', async (req, res) => {
+    const { code } = req.query;
+    res.send(`Authorization code received: ${code}`);
+
+    const bodyParams = new URLSearchParams({
+        client_id : 'fullcycle-client',
+        grant_type : 'authorization_code',
+        code: req.query.code as string,
+        redirect_uri : 'http://localhost:3000/callback'
+    });
+
+    const url = `http://host.docker.internal:8080/realms/fullcycle-realm/protocol/openid-connect/token`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: bodyParams.toString()
+    })
+    
+
+
+    const result = await response.json();
+    console.log(result)
+    res.json(result);
+
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
